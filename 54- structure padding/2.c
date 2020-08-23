55fc7a5a7a85d86f275427b0c7d7d76a4f979508
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-08-24 01:22:40 +0200

    structure padding

---
+++ 54- structure padding/2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#pragma pack(1)
+
+typedef struct Student1
+{
+    char a;
+    char b;
+    int c;
+
+} Student1;
+
+typedef struct Student2
+{
+    char a;
+    int c;
+    char b;
+
+} Student2;
+
+typedef struct Student3
+{
+    char a;
+    long c;
+    char b;
+
+} Student3;
+
+
+int main()
+{
+    printf("%ld\n", sizeof(Student1));
+    printf("%ld\n", sizeof(Student2));
+    printf("%ld\n", sizeof(Student3));
+
+    return 0;
+}
